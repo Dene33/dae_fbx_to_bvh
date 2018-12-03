7ffbcfcd41129e05e682e5331a47bf407514feb5
Dene <den33.info@gmail.com>
2018-12-03 23:46:20 +0200

    Add files via upload

---
+++ dae_fbx_to_bvh.py
@@ -0,0 +1,54 @@
+import bpy
+import os
+
+def export_bvh(file):
+    bpy.ops.object.select_all(action='TOGGLE')
+  
+    obj = scene.objects.get (bpy.data.armatures[0].name)
+    if obj != None:
+        obj.select = True
+        scene.objects.active = obj
+        
+    #cont = bpy.context.area.type
+    #print(str(cont))
+                        
+    bpy.ops.export_anim.bvh(filepath=directory + '\\' + file[:-4] + '.bvh')
+    
+    bpy.ops.object.select_all(action='TOGGLE')
+    bpy.ops.object.select_all(action='TOGGLE')
+
+    bpy.ops.object.delete(use_global=False)
+    print('Exported:'+file)
+
+scene = bpy.context.scene
+    
+filepath = bpy.data.filepath
+directory = os.path.dirname(filepath)
+
+for obj in bpy.context.scene.objects:
+    if obj.type == 'ARMATURE':
+        obj.select = True
+    else:
+        obj.select = False
+bpy.ops.object.delete()
+
+for block in bpy.data.armatures:
+    if block.users == 0:
+        bpy.data.armatures.remove(block)
+        
+for block in bpy.data.actions:
+    if block.users == 0:
+        bpy.data.actions.remove(block)
+
+for file in os.listdir(directory):
+    if file.endswith('.dae'):
+        bpy.ops.wm.collada_import(filepath=directory+'\\' + file,
+                                  fix_orientation=True,
+                                  find_chains=True)
+        export_bvh(file)
+        
+    elif file.endswith('.fbx'):
+        bpy.ops.import_scene.fbx(filepath = directory+'\\' + file,
+                                 automatic_bone_orientation=True,
+                                 ignore_leaf_bones=True)
+        export_bvh(file)
---
+++ dae_fbx_to_bvh.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+blender --background  dae_fbx_to_bvh.blend -noaudio -P dae_fbx_to_bvh.py
